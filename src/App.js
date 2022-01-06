b3f73779fc4004f448232427313d43b68227c602
AlexanderInglis <alexinglis10@gmail.com>
2022-01-05 18:17:39 -0800

    added cursor to scene

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useRef } from "react";
 import "./App.scss";
 import GravityScene from "./scene/gravityScene";
+import Cursor from "./Cursor";
 
 function App() {
   return (
     <>
+      <Cursor />
       <GravityScene />
     </>
   );
