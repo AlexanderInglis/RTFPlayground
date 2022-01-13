839a82fb836c69c5c688c3f128a82b7c0d148321
AlexanderInglis <alexinglis10@gmail.com>
2022-01-12 17:58:30 -0800

    made it work

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import GravityScene from "./scene/gravityScene";
 import Cursor from "./assets/Cursor";
 import ImageScene from "./scene/imageScene";
+import HorizontalScroller from "./scene/Horizontal/index";
 
 function App() {
   return (
     <>
-      <GravityScene />
+      {/* <GravityScene /> */}
       {/* <ImageScene /> */}
+      <HorizontalScroller />
       <Cursor />
     </>
   );
